abe8e94cf2e2909e1bd907e070f98f6a323d8aae
Emir Şerafet Cam <101120537+EmirSerafetCan@users.noreply.github.com>
2022-05-18 16:03:34 +0300

    Add files via upload

---
+++ List-Mutable-Collection.kt
@@ -0,0 +1,104 @@
+fun main(Args: Array<String>) {
+
+/*
+    println("Sayı Giriniz")
+    var sayi = readLine()!!.toInt()
+    print(sayi)
+
+    var onetoTen = 1..10
+    println(5 in onetoTen)
+    println(12 in onetoTen)
+
+    var isim = "Serafet"
+    for (karakter in isim)
+    println(karakter)
+
+    var isimDizisi = ArrayList<String>()
+
+    isimDizisi.add("Caner")
+    isimDizisi.add("Emir")
+    isimDizisi.add("Arife")
+    isimDizisi.add("Canan")
+
+    println("1.İsim: "+isimDizisi.get(0))
+    println("2.İsim: "+isimDizisi.get(1))
+    println("3.İsim: "+isimDizisi.get(2))
+    println("4.İsim: "+isimDizisi.get(3))
+    isimDizisi.set(1,"Serafet")
+    println("2.İsim: "+isimDizisi.get(1))
+
+
+    //List'ler index ile erişilebilen sıralı Collectionlardır.
+    val Takımlar = listOf("Fenerbahçe", null, "Galatasaray", "Beşiktaş")
+    println(Takımlar)
+
+    val Takımlar1 = arrayListOf("Fenerbahçe", "Galatasaray", "Beşiktaş")
+    Takımlar1.add(1,"Trabzonspor")                              //arrayListOf -> ekleme yapılabilir.
+    println(Takımlar1)
+
+    val Takımlar2 = mutableListOf("Fenerbahçe", null, "Galatasaray", "Beşiktaş")
+    Takımlar2.removeAt(3)
+    Takımlar2[1] = "Giresunspor"         //mutableListOf -> ekleme, silme ve güncelleme yapılabilir.
+    println(Takımlar2)
+
+    //Set'ler birbirinden benzersiz Collectionlardır. Bir elemandan bir kez bulunabilir.
+    //Map'ler key-value şeklindeki Collectinolardır. Her key benzersizdir ancak her valuenin eşsiz olmasına gerek yoktur
+
+    val diziMap = mutableMapOf<Int, String>(1 to "Halil", 2 to "Murat", 3 to "Ömer")
+    println(diziMap)
+
+    diziMap.put(4, "Tunahan")
+    diziMap[5] = "Rıdvan"
+    diziMap.remove(1)
+    diziMap[3] = "Faruk"
+    println(diziMap)
+
+    var isimDizisi = ArrayList<String>()
+
+    isimDizisi.add("Caner")
+    isimDizisi.add("Emir")
+    isimDizisi.add("Arife")
+    isimDizisi.add("Canan")
+
+    println(isimDizisi.contains("Caner"))    //contains -> girilen değere göre true false döner.
+    println(isimDizisi.contains("Canerr"))
+
+    println(isimDizisi.indexOf("Arife"))     //indexOf -> girilen değer varsa indexini döner yoksa -1 döner.
+    println(isimDizisi.indexOf("Canann"))
+
+
+    val sayıListe = mutableListOf(1, 2, 3, 4, 5, 6)
+
+    println(sayıListe.any { it % 2 == 0 })
+    println(sayıListe.any { it > 10 })           //any -> verilen koşula uygun elemana göre true false döner.
+
+    println(sayıListe.count())
+    println(sayıListe.count { it % 2 == 0 })     //count -> verilen koşula uygun elemanı döner.
+
+    println(sayıListe.filter { it % 3 == 0 })    //filter -> verilen koşula göre filtre yapar.
+
+    println(sayıListe.contains(7))       //contains -> girilen eleman listede varsa true yoksa f döner.
+
+    println(sayıListe.maxOrNull())       //en büyük sayıyı döner eleman yoksa null döner
+
+    println(sayıListe.first { it % 2 == 0 })
+    println(sayıListe.last { it % 2 == 0 })    //verilen koşula uygun ilk elemanı first, son elemanı last döner
+
+    println(sayıListe.indexOfFirst { it % 3 == 0 })     //verilen koşula uygun ilk elemanın indeksini first,
+    println(sayıListe.indexOfLast { it % 3 == 0 })      //son elemanın indeksini last döner
+
+    println(sayıListe.indexOf(1))      //gir. elem. varsa indeksi döner yoksa -1 döner
+    println(sayıListe.indexOf(7))
+
+    println(sayıListe.reversed())   //diziyi ters çevirir
+
+    println(sayıListe.sortedDescending())   //diziyi büyükten küçüğe sıralar
+
+    println(sayıListe.groupBy { if (it % 2 == 0) "Çift" else "Tek" })   //verilen koşula göre gruplar
+
+    println(sayıListe.elementAt(3))
+    println(sayıListe.elementAt(6))   //girilen indeksteki elemanı döner yoksa hata verir
+
+*/
+
+}
---
+++ İfElse-When-TryCatch-For-ForEach.kt
@@ -0,0 +1,91 @@
+fun main(Args: Array<String>) {
+
+/*
+    val sayi = 12
+    val sonuc = if (sayi % 2 == 0) {
+        "Sayı Çifttir"
+    }
+    else {
+        "Sayı Tektir"
+    }
+    println(sonuc)
+
+    val sayi1 = 7
+    val sonuc1 = if (sayi1 % 2 == 1)"Sayı Tek" else "Sayı Çift"
+    println(sonuc1)
+
+    val liste = mutableListOf<Int>(1,2,3)   // ?=ELVİS OPERATÖRÜ -> ? solundaki durum geçerliyse
+    val boyut = liste?.size?:-1             //değişkene sol kısmı atar, değilse sağ kısmı atar.
+    println(boyut)
+
+    var sayi = 2
+    when(sayi) {
+        2 ->
+        {
+            println("Sayı 2'dir")           //when = switch-case
+        }                                   //if-else ile yapılan her şey when ile yapılabilir
+        5 ->                                //when kullanımı if-else göre daha kolaydır.
+        {
+            println("Sayı 5'dir")
+        }
+        else ->
+        {
+            println("Sayı ne 2 ne 5'dir.")
+        }
+    }
+
+    try {
+        var sayi = 10/0
+        println("İşlem Yapılıyor")
+        println(sayi)
+    }
+    catch (e : ArithmeticException) {
+        println("ArithmeticException Hatası")
+    }
+
+    finally {
+        //Bu blok her zaman çalışır.
+    }
+
+    repeat(10)
+    {
+        println("index :" +it)    //repeat -> girilen değer kadar sayı döner. 0dan başlar. Sayılara it ile erişilir.
+    }
+
+    for (x in 1..10)
+        println("x: " +x)
+
+    for (y in 1..10 step 2)
+        println("y: " +y)
+
+    for (z in 10 downTo 1)
+        println("z: " +z)              //down to -> geri geri döner
+
+    for (k in 1 until 10)
+        println("k: " +k)           //until sonuncu sayıyı dahil etmez
+
+    for (a in "Medipol")
+        println("a: " +a)
+
+    val isimler = arrayOf("A", "B", "C")
+    for (b in isimler)
+        println("b: " +b)
+
+    val isimler1 = arrayOf("A", "B", "C")
+    for (c in isimler.withIndex())
+        println("c: " +c)
+
+    (1..10).forEach { x-> println("x: " +x) }
+
+    val liste = mutableListOf("Can", "Efe", "Emir")
+    liste.forEach{ liste -> println("isim: " +liste) }
+
+    for (x in 1..10) {
+        println("x: " + x)
+        if (x == 3) {
+            break
+        }
+    }
+    */
+
+}
